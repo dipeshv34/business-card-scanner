46356d7d158b9135e104004de2fbdd97577e022a
Harshal Faldu <harshalf.wot2021@gmail.com>
2023-03-31 09:17:40 +0530

    [feat] Dynamically show form on api response

--- controllers/ScannerController.js
+++ controllers/ScannerController.js
@@ -20,24 +20,26 @@ class ScannerController extends BaseController {
   async scanDocument(req, res) {
     try {
       console.log(req.files)
+      let data = ''
       if (req.files.length > 0) {
 
-        this.tesseract.recognize(
+        data = await this.tesseract.recognize(
           // this first argument is for the location of an image it can be a //url like below or you can set a local path in your computer
-          'public\\documentToScan\\test.jpg',
+          req.files[0].path,
           // this second argument is for the laguage
           'eng',
           { logger: m => console.log("logger", m) }
-        ).then(({ data: { text } }) => {
-          console.log("text ->>>>>>>>>>", text);
-        })
+        )
       }
 
       // if(this.fs.existsSync(req.files[0].path)){
       //   this.fs.unlinkSync(req.files[0].path)
       // }
 
-      return res.send()
+      return res.send({
+        status: true,
+        data
+      })
     } catch (e) {
       console.log("error =>>>>>>>>>>>>>>", e.message)
     }
